ed272760bdf12710b8c1dc707ea82afc70561791
nawinlaxman <nawin27laxman@gmail.com>
2022-06-19 13:31:31 +0530

    jenkins

--- src/main/java/com/salesforce/pages/LandingPage.java
+++ src/main/java/com/salesforce/pages/LandingPage.java
@@ -1,8 +1,20 @@
 package com.salesforce.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 
-public class LandingPage {
+import com.framework.testng.api.base.ProjectSpecificMethods;
+
+public class LandingPage extends ProjectSpecificMethods {
+	
+	public LandingPage verifypagetitle(String data) {
+		
+		pause(5000);
+		verifyTitle(data);
+		return this;
+		
+	}
+	
 	
 
 }
--- src/main/java/com/salesforce/pages/LoginPage.java
+++ src/main/java/com/salesforce/pages/LoginPage.java
@@ -2,7 +2,28 @@ package com.salesforce.pages;
 
 import org.openqa.selenium.WebDriver;
 
-public class LoginPage {
-	
+import com.framework.selenium.api.design.Locators;
+import com.framework.testng.api.base.ProjectSpecificMethods;
 
+public class LoginPage extends ProjectSpecificMethods {
+	
+	public LoginPage enterUserName(String data) {
+		
+		clearAndType(locateElement(Locators.ID, "username"), data);
+		return this;
+	}
+	
+	public LoginPage enterPassword(String data) {
+		
+		clearAndType(locateElement(Locators.ID, "password"), data);
+		return this;
+	}
+	
+	public LandingPage clickLogin() {
+		click(locateElement(Locators.ID, "Login"));
+		return new LandingPage();
+	}
+		
+	
+	
 }
